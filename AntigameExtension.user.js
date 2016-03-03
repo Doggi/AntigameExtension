a15b36dcc78390d9f04540f1b47f902f1a16fc78
Sören Grzanna <soeren.grzanna@stud.hs-hannover.de>
2016-03-03 02:28:33 +0100

    Initial commit

---
+++ AntigameExtension.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name        AntigameExtension
+// @namespace   doggi.ogame.antigame.extension
+// @include     http*de.ogame.gameforge.com*
+// @include     http*google*
+// @version     1
+// @grant       GM_deleteValue
+// @grant       GM_getValue
+// @grant       GM_listValues
+// @grant       GM_setValue
+// @grant       GM_getResourceText
+// @grant       GM_getResourceURL
+// @grant       GM_addStyle
+// @grant       GM_openInTab
+// @grant       GM_registerMenuCommand
+// @grant       GM_setClipboard
+// @grant       GM_xmlhttpRequest
+//
+// require      https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js
+// ==/UserScript==
+console.log("AntigameExtension starting");
+console.log($);
+console.log("test");
